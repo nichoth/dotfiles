1ff6dea5d5f8d1986abdb704ff7b46cbb7c9005c
nichoth <nichoth@gmail.com>
2015-06-27 19:45:15 -0700

    tweak

--- simple-server.js
+++ simple-server.js
@@ -2,7 +2,7 @@ var http = require('http'),
     ecstatic = require('ecstatic');
 
 http.createServer(
-  ecstatic({ root: __dirname + '/example' })
+  ecstatic({ root: __dirname + '/public' })
 ).listen(8000);
 
 console.log('Listening on :8000');
